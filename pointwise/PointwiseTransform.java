f349374c3a92f8e9ad6e85a10a4ee51c4544d861
Lucas da Costa <lucasdacosta@usp.br>
2023-08-28 17:29:32 -0300

    Practice of the week

--- pointwise/PointwiseTransform.java
+++ pointwise/PointwiseTransform.java
@@ -1,4 +1,3 @@
-
 public class PointwiseTransform extends Object {
 
 	/**
@@ -22,12 +21,26 @@ public class PointwiseTransform extends Object {
 	* Question 2.2 Stretch normalized constrast
 	*/
 	static public ImageAccess rescale(ImageAccess input) {
+	    // Get Image with / height
 		int nx = input.getWidth();
 		int ny = input.getHeight();
+
+		// Calculate alpha coefficient
 		double max = input.getMaximum();
 		double min = input.getMinimum();
-		ImageAccess output = new ImageAccess(nx, ny);
-		// Add your code here
+	    double alpha = 255.0 / (max - min);
+	    
+		// Calculate beta coefficient
+	    double beta = min;
+	
+	    ImageAccess output = new ImageAccess(nx, ny);
+	
+	    double value = 0.0;
+	    for (int x=0; x<nx; x++)
+		for (int y=0; y<ny; y++) {
+			value = alpha * (input.getPixel(x, y) - beta);
+			output.putPixel(x, y, value);
+		}
 		return output;	
 	}
 
@@ -35,10 +48,21 @@ public class PointwiseTransform extends Object {
 	* Question 2.3 Saturate an image
 	*/
 	static public ImageAccess saturate(ImageAccess input) {
+		// Get width / Height
 		int nx = input.getWidth();
 		int ny = input.getHeight();
+		
 		ImageAccess output = new ImageAccess(nx, ny);
-		// Add your code here
+	
+		// If pixel value is more than 10000, saturate the pixel  
+		for (int x=0; x<nx; x++)
+		for (int y=0; y<ny; y++) {
+			if (input.getPixel(x, y) > 10000) {
+				output.putPixel(x, y, 10000);
+			} else {
+				output.putPixel(x, y, input.getPixel(x, y));
+			}
+		}
 		return output;
 	}
 	
@@ -46,11 +70,26 @@ public class PointwiseTransform extends Object {
 	* Question 4.1 Maximum Intensity Projection
 	*/
 	static public ImageAccess zprojectMaximum(ImageAccess[] zstack) {
-		int nx = zstack[0].getWidth();
+		// Get width / height and Z
+    	int nx = zstack[0].getWidth();
 		int ny = zstack[0].getHeight();
 		int nz = zstack.length;
-		ImageAccess output = new ImageAccess(nx, ny);
-		// Add your code here
+		
+    	ImageAccess output = new ImageAccess(nx, ny);
+	  
+    	double value_max = 0.0;
+    	for (int x=0; x<nx; x++)
+		for (int y=0; y<ny; y++) {
+    	  	value_max = zstack[0].getPixel(x, y);
+    	  	// Iterate between Z axis
+    	  	for (int z=0; z<nz; z++){
+				if (value_max < zstack[z].getPixel(x, y)) {
+    	      	value_max = zstack[z].getPixel(x, y);
+				}
+    	  	}
+
+			output.putPixel(x, y, value_max);
+    	}
 		return output;	
 	}
 
@@ -61,8 +100,19 @@ public class PointwiseTransform extends Object {
 		int nx = zstack[0].getWidth();
 		int ny = zstack[0].getHeight();
 		int nz = zstack.length;
+		
 		ImageAccess output = new ImageAccess(nx, ny);
-		// Add your code here
+			
+    	double z_accumulator = 0.0;
+    	for (int x=0; x<nx; x++)
+		for (int y=0; y<ny; y++) {
+			z_accumulator = 0.0;
+    		for (int z=0; z<nz; z++){
+    	    	z_accumulator += zstack[z].getPixel(x, y); 
+		  	}
+			output.putPixel(x, y, z_accumulator/nz);
+    	}
+	
 		return output;	
 	}
 
